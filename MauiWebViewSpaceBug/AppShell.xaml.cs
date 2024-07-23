e57751f311ab413f547985119782e537a70f502a
Costas <Costas@LAPTOP-11THEHBB>
2024-07-23 13:19:38 +0300

    Add project files.

---
+++ MauiWebViewSpaceBug/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiWebViewSpaceBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiWebViewSpaceBug/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiWebViewSpaceBug
+{
+    public partial class MainPage : ContentPage
+    {
+        string currentUrl;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void sendQuery_Clicked(object sender, EventArgs e)
+        {
+            bugTestWebView.Source = "https://example.com/?q=" + query.Text;
+        }
+    }
+
+}
